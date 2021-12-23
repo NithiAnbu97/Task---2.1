7b34c434813c15bd85e9ca5bd5ec4185c63549df
NithiAnbu97 <96058658+NithiAnbu97@users.noreply.github.com>
2021-12-23 13:38:33 +0530

    Add files via upload

---
+++ task 2.1.js
@@ -0,0 +1,13 @@
+task 2
+     console.log(typeof (1));
+    console.log(typeof (1.1));
+     console.log(typeof "1.1");
+     console.log(typeof (true));       
+       console.log(typeof (null));       
+     console.log(typeof (undefined));       
+     console.log(typeof [1,2,3]);       
+    
+    console.log(typeof { b : 1});       
+    
+   console.log(typeof (NaN));       
+    
